32482f3aa6d46d6353e481fb9793b0d976f694bb
lyq <524123601@qq.com>
2020-07-23 15:22:48 +0800

    2020-07-23 15:22:48.70

--- generateCatalogue.py
+++ generateCatalogue.py
@@ -2,15 +2,15 @@ import os
 import re
 
 with open('./README.md', 'w', encoding='utf_8') as f:
-	fileWrite = ['## Catalogue\n']
+	fileWrite = ['# Catalogue\n']
 	directories = os.listdir()
 	for d in directories:
 		if os.path.isdir(d) and d[0] not in ['_', '.']:
-			fileWrite.append('* {}\n'.format(d))
+			fileWrite.append('## {}\n'.format(d))
 			mdList = os.listdir(d)
 			for md in mdList:
 				matchFileName = re.match(r'^「(.+)」(.+)\.md$', md)
 				tag = matchFileName.group(1)
 				title = matchFileName.group(2)
-				fileWrite.append('\t* [{} - {}]({}/{})\n'.format(tag, title, d, md))
+				fileWrite.append('- [{} - {}]({}/{})\n'.format(tag, title, d, md))
 	f.writelines(fileWrite)
