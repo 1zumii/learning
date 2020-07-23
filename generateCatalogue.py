ee83f4a5fa2e1488e233233763e8ae521cd20728
lyq <524123601@qq.com>
2020-07-23 15:15:23 +0800

    2020-07-23 15:15:23.15

--- generateCatalogue.py
+++ generateCatalogue.py
@@ -9,9 +9,8 @@ with open('./README.md', 'w', encoding='utf_8') as f:
 			fileWrite.append('* {}\n'.format(d))
 			mdList = os.listdir(d)
 			for md in mdList:
-				matchFileName = re.match(r'^(.+)\s-\s(.+)\.md$', md)
+				matchFileName = re.match(r'^「(.+)」(.+)\.md$', md)
 				tag = matchFileName.group(1)
 				title = matchFileName.group(2)
-				# fileWrite.append('\t* [「{}」{}]({}/{})\n'.format(tag, title, d, md))
-				fileWrite.append('\t* [{} - {}]({}/{})\n'.format(tag, title, d, md))
+				fileWrite.append('\t* [{}: {}]({}/{})\n'.format(tag, title, d, md))
 	f.writelines(fileWrite)
