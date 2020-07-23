97797b5f9798d700e26a3c1491334ad8c30884e9
lyq <524123601@qq.com>
2020-07-23 14:38:20 +0800

    2020-07-23 14:38:20.01

--- generateCatalogue.py
+++ generateCatalogue.py
@@ -2,7 +2,7 @@ import os
 import re
 
 with open('./README.md', 'w', encoding='utf_8') as f:
-	fileWrite = ['## Catalogue\n']
+	fileWrite = ['## Catalogue\n', '- [kkk](generateCatalogue.py)']
 	directories = os.listdir()
 	for d in directories:
 		if os.path.isdir(d) and d[0] not in ['_', '.']:
@@ -12,6 +12,5 @@ with open('./README.md', 'w', encoding='utf_8') as f:
 				matchFileName = re.match(r'^(.+)\s-\s(.+)\.md$', md)
 				tag = matchFileName.group(1)
 				title = matchFileName.group(2)
-				# fileWrite.append('\t- [「{}」{}]({}/{})\n'.format(tag, title, d, md))
-				fileWrite.append('\t- [{} - {}]({}/{})\n'.format(tag, title, d, md))
+				fileWrite.append('\t- [「{}」{}]({}/{})\n'.format(tag, title, d, md))
 	f.writelines(fileWrite)
--- git_push.bat
+++ git_push.bat
@@ -1,3 +1,4 @@
+python generateCatalogue.py
 git add --all
 git commit -m "%date:~0,4%-%date:~5,2%-%date:~8,2% %time%"
 git push
